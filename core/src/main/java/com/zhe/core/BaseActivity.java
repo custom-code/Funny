f4b82fa53bf31081b342bebb452b292bbfd01909
zhe <tianxiaoxie@126.com>
2016-05-21 02:38:43 +0800

    first commit

---
+++ core/src/main/java/com/zhe/core/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.zhe.core;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhe on 16/5/5.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
